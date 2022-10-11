cd29c0d11148ff1c1f9f2ae3998f431eaa47b5d1
Jakub Trllo <jakub.trllo@gmail.com>
2022-10-11 15:43:49 +0200

    change main functionality

--- services/processor/processor/__main__.py
+++ services/processor/processor/__main__.py
@@ -1,51 +1,29 @@
-import time
-import ayclient
-from nxtools import logging
+import os
+
+from ayclient import addon_settings
+from .ftrack_session import OPServerSession
+from .ftrack_server import FtrackServer
 
 
-def get_job():
-    payload = {
-        "sourceTopic": "ftrack.leech",
-        "targetTopic": "ftrack.proc",
-        "sender": ayclient.config.service_name,
-        "description": "Event processing",
-        "sequential": True,
-    }
-    response = ayclient.api.post("enroll", json=payload)
-    if not response:
-        if response.status_code == 404:
-            logging.info("Nothing to do")
-        else:
-            logging.error("Something's wrong")
-        return None
-    return response.json()
-
-
-def process(event_id: str, source_id: str):
-
-    source_event = ayclient.api.get(f"events/{source_id}").json()
-    print("Source event:", source_event)
-
-    description = f"Processed {source_event['description']}"
-
-    req_data = {
-        "sender": ayclient.config.service_name,
-        "description": description,
-        "payload": {"value": "whatevr"},
-        "status": "finished",
-    }
-
-    res = ayclient.api.patch(f"events/{event_id}", json=req_data)
-    assert res
+def get_handler_paths() -> list[str]:
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    handler_paths = [
+        os.path.join(current_dir, "base_handlers"),
+    ]
+    return handler_paths
 
 
 def main():
-    while True:
-        job = get_job()
-        if job is None:
-            break
-        process(job["id"], job["dependsOn"])
-        time.sleep(0.01)
+    handler_paths = get_handler_paths()
+    settings = addon_settings()
+    service_settings = settings["service_settings"]
+    session = OPServerSession(
+        settings["ftrack_url"],
+        service_settings["api_key"],
+        service_settings["username"]
+    )
+    server = FtrackServer(handler_paths)
+    server.run_server(session)
 
 
 if __name__ == "__main__":
