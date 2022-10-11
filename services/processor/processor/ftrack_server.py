5eb1adf64bd7907a4fe75fdc9a69a87a2f9cd093
Jakub Trllo <jakub.trllo@gmail.com>
2022-10-11 15:43:27 +0200

    added stupidly basic implementation of event handlers

---
+++ services/processor/processor/ftrack_server.py
@@ -0,0 +1,244 @@
+import os
+import sys
+import time
+import types
+import logging
+import traceback
+import importlib
+
+import ftrack_api
+
+"""
+# Required - Needed for connection to Ftrack
+FTRACK_SERVER # Ftrack server e.g. "https://myFtrack.ftrackapp.com"
+FTRACK_API_KEY # Ftrack user's API key "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+FTRACK_API_USER # Ftrack username e.g. "user.name"
+
+# Required - Paths to folder with actions
+FTRACK_ACTIONS_PATH # Paths to folders where are located actions
+    - EXAMPLE: "M:/FtrackApi/../actions/"
+FTRACK_EVENTS_PATH # Paths to folders where are located actions
+    - EXAMPLE: "M:/FtrackApi/../events/"
+
+# Required - Needed for import included modules
+PYTHONPATH # Path to ftrack_api and paths to all modules used in actions
+    - path to ftrack_action_handler, etc.
+"""
+
+
+def import_filepath(filepath, module_name=None):
+    """Import python file as python module.
+
+    Python 2 and Python 3 compatibility.
+
+    Args:
+        filepath(str): Path to python file.
+        module_name(str): Name of loaded module. Only for Python 3. By default
+            is filled with filename of filepath.
+    """
+    if module_name is None:
+        module_name = os.path.splitext(os.path.basename(filepath))[0]
+
+    # Make sure it is not 'unicode' in Python 2
+    module_name = str(module_name)
+
+    # Prepare module object where content of file will be parsed
+    module = types.ModuleType(module_name)
+
+    # Use loader so module has full specs
+    module_loader = importlib.machinery.SourceFileLoader(
+        module_name, filepath
+    )
+    module_loader.exec_module(module)
+    return module
+
+
+def modules_from_path(folder_path, log=None):
+    """Get python scripts as modules from a path.
+
+    Arguments:
+        path (str): Path to folder containing python scripts.
+
+    Returns:
+        tuple<list, list>: First list contains successfully imported modules
+            and second list contains tuples of path and exception.
+    """
+    crashed = []
+    modules = []
+    output = (modules, crashed)
+    # Just skip and return empty list if path is not set
+    if not folder_path:
+        return output
+
+    if log is None:
+        log = logging.getLogger("modules_from_path")
+    # Do not allow relative imports
+    if folder_path.startswith("."):
+        log.warning((
+            "BUG: Relative paths are not allowed for security reasons. {}"
+        ).format(folder_path))
+        return output
+
+    folder_path = os.path.normpath(folder_path)
+
+    if not os.path.isdir(folder_path):
+        log.warning("Not a directory path: {}".format(folder_path))
+        return output
+
+    for filename in os.listdir(folder_path):
+        # Ignore files which start with underscore
+        if filename.startswith("_"):
+            continue
+
+        mod_name, mod_ext = os.path.splitext(filename)
+        if not mod_ext == ".py":
+            continue
+
+        full_path = os.path.join(folder_path, filename)
+        if not os.path.isfile(full_path):
+            continue
+
+        try:
+            module = import_filepath(full_path, mod_name)
+            modules.append((full_path, module))
+
+        except Exception:
+            crashed.append((full_path, sys.exc_info()))
+            log.warning(
+                "Failed to load path: \"{0}\"".format(full_path),
+                exc_info=True
+            )
+            continue
+
+    return output
+
+
+class FtrackServer:
+    def __init__(self, handler_paths=None):
+        """
+            - 'type' is by default set to 'action' - Runs Action server
+            - enter 'event' for Event server
+
+            EXAMPLE FOR EVENT SERVER:
+                ...
+                server = FtrackServer()
+                server.run_server()
+                ..
+        """
+
+        # set Ftrack logging to Warning only - OPTIONAL
+        ftrack_log = logging.getLogger("ftrack_api")
+        ftrack_log.setLevel(logging.WARNING)
+
+        self.log = logging.getLogger(__name__)
+
+        self.stopped = True
+        self.is_running = False
+
+        self.handler_paths = handler_paths or []
+
+    def stop_session(self):
+        self.stopped = True
+        if self.session.event_hub.connected is True:
+            self.session.event_hub.disconnect()
+        self.session.close()
+        self.session = None
+
+    def set_files(self, paths):
+        # Iterate all paths
+        register_functions = []
+        for path in paths:
+            # Try to format path with environments
+            try:
+                path = path.format(**os.environ)
+            except BaseException:
+                pass
+
+            # Get all modules with functions
+            modules, crashed = modules_from_path(path)
+            for filepath, exc_info in crashed:
+                self.log.warning("Filepath load crashed {}.\n{}".format(
+                    filepath, traceback.format_exception(*exc_info)
+                ))
+
+            for filepath, module in modules:
+                register_function = None
+                for name, attr in module.__dict__.items():
+                    if (
+                        name == "register"
+                        and isinstance(attr, types.FunctionType)
+                    ):
+                        register_function = attr
+                        break
+
+                if not register_function:
+                    self.log.warning(
+                        "\"{}\" - Missing register method".format(filepath)
+                    )
+                    continue
+
+                register_functions.append(
+                    (filepath, register_function)
+                )
+
+        if not register_functions:
+            self.log.warning((
+                "There are no events with `register` function"
+                " in registered paths: \"{}\""
+            ).format("| ".join(paths)))
+
+        for filepath, register_func in register_functions:
+            try:
+                register_func(self.session)
+            except Exception:
+                self.log.warning(
+                    "\"{}\" - register was not successful".format(filepath),
+                    exc_info=True
+                )
+
+    def set_handler_paths(self, paths):
+        self.handler_paths = paths
+        if self.is_running:
+            self.stop_session()
+            self.run_server()
+
+        elif not self.stopped:
+            self.run_server()
+
+    def run_server(self, session=None, load_files=True):
+        self.stopped = False
+        self.is_running = True
+        if not session:
+            session = ftrack_api.Session(auto_connect_event_hub=True)
+
+        # Wait until session has connected event hub
+        if session._auto_connect_event_hub_thread:
+            # Use timeout from session (since ftrack-api 2.1.0)
+            timeout = getattr(session, "request_timeout", 60)
+            started = time.time()
+            while not session.event_hub.connected:
+                if (time.time() - started) > timeout:
+                    raise RuntimeError((
+                        "Connection to Ftrack was not created in {} seconds"
+                    ).format(timeout))
+                time.sleep(0.1)
+
+        self.session = session
+        if load_files:
+            if not self.handler_paths:
+                self.log.warning((
+                    "Paths to event handlers are not set."
+                    " Ftrack server won't launch."
+                ))
+                self.is_running = False
+                return
+
+            self.set_files(self.handler_paths)
+
+            msg = "Registration of event handlers has finished!"
+            self.log.info(len(msg) * "*")
+            self.log.info(msg)
+
+        # keep event_hub on session running
+        self.session.event_hub.wait()
+        self.is_running = False
---
+++ services/processor/processor/ftrack_session.py
@@ -0,0 +1,287 @@
+import os
+import sys
+import logging
+import getpass
+import atexit
+import threading
+import time
+import collections
+import appdirs
+
+import requests
+import ftrack_api
+import ftrack_api.session
+import ftrack_api.cache
+import ftrack_api.operation
+import ftrack_api._centralized_storage_scenario
+import ftrack_api.event
+
+from weakref import WeakMethod
+
+import ayclient
+
+TOPIC_STATUS_SERVER = "openpype.event.server.status"
+TOPIC_STATUS_SERVER_RESULT = "openpype.event.server.status.result"
+
+
+class ProcessEventHub(ftrack_api.event.hub.EventHub):
+    _server_con = None
+
+    def prepare_server_connection(self):
+        try:
+            assert ayclient.get("users/me")
+
+        except Exception:
+            self.log.error(
+                "Server \"{}\" is not responding, exiting."
+            )
+            sys.exit(0)
+
+    def get_next_ftrack_event(self):
+        payload = {
+            "sourceTopic": "ftrack.leech",
+            "targetTopic": "ftrack.proc",
+            "sender": ayclient.config.service_name,
+            "description": "Event processing",
+            "sequential": True,
+        }
+        response = ayclient.api.post("enroll", json=payload)
+        if not response:
+            if response.status_code == 404:
+                logging.info("Nothing to do")
+            else:
+                logging.error("Something's wrong")
+            return None
+        return response.json()
+
+    def finish_job(self, job):
+        event_id = job["id"]
+        source_id = job["dependsOn"]
+        source_event = ayclient.api.get(f"events/{source_id}").json()
+        print("Source event:", source_event)
+
+        description = f"Processed {source_event['description']}"
+
+        req_data = {
+            "sender": ayclient.config.service_name,
+            "description": description,
+            "payload": {"value": "whatevr"},
+            "status": "finished",
+        }
+
+        res = ayclient.api.patch(f"events/{event_id}", json=req_data)
+        assert res
+
+    def wait(self, duration=None):
+        """Overridden wait
+        Event are loaded from Mongo DB when queue is empty. Handled event is
+        set as processed in Mongo DB.
+        """
+        started = time.time()
+        self.prepare_server_connection()
+        while True:
+            job = self.get_next_ftrack_event()
+            if not job:
+                time.sleep(0.1)
+                continue
+
+            src_job = ayclient.api.get(f"events/{job['dependsOn']}").json()
+            ftrack_event = src_job["payload"]
+            self._handle(ftrack_event)
+
+            self.finish_job(job)
+
+            # Additional special processing of events.
+            if ftrack_event['topic'] == 'ftrack.meta.disconnected':
+                break
+
+            if duration is not None:
+                if (time.time() - started) > duration:
+                    break
+
+    def _handle_packet(self, code, packet_identifier, path, data):
+        """Override `_handle_packet` which skip events and extend heartbeat"""
+        code_name = self._code_name_mapping[code]
+        if code_name == "event":
+            return
+
+        return super()._handle_packet(code, packet_identifier, path, data)
+
+
+class CustomEventHubSession(ftrack_api.session.Session):
+    '''An isolated session for interaction with an ftrack server.'''
+    def __init__(
+        self, server_url=None, api_key=None, api_user=None, auto_populate=True,
+        plugin_paths=None, cache=None, cache_key_maker=None,
+        auto_connect_event_hub=False, schema_cache_path=None,
+        plugin_arguments=None, timeout=60, **kwargs
+    ):
+        self.kwargs = kwargs
+
+        super(ftrack_api.session.Session, self).__init__()
+        self.logger = logging.getLogger(
+            __name__ + '.' + self.__class__.__name__
+        )
+        self._closed = False
+
+        if server_url is None:
+            server_url = os.environ.get('FTRACK_SERVER')
+
+        if not server_url:
+            raise TypeError(
+                'Required "server_url" not specified. Pass as argument or set '
+                'in environment variable FTRACK_SERVER.'
+            )
+
+        self._server_url = server_url
+
+        if api_key is None:
+            api_key = os.environ.get(
+                'FTRACK_API_KEY',
+                # Backwards compatibility
+                os.environ.get('FTRACK_APIKEY')
+            )
+
+        if not api_key:
+            raise TypeError(
+                'Required "api_key" not specified. Pass as argument or set in '
+                'environment variable FTRACK_API_KEY.'
+            )
+
+        self._api_key = api_key
+
+        if api_user is None:
+            api_user = os.environ.get('FTRACK_API_USER')
+            if not api_user:
+                try:
+                    api_user = getpass.getuser()
+                except Exception:
+                    pass
+
+        if not api_user:
+            raise TypeError(
+                'Required "api_user" not specified. Pass as argument, set in '
+                'environment variable FTRACK_API_USER or one of the standard '
+                'environment variables used by Python\'s getpass module.'
+            )
+
+        self._api_user = api_user
+
+        # Currently pending operations.
+        self.recorded_operations = ftrack_api.operation.Operations()
+
+        # OpenPype change - In new API are operations properties
+        new_api = hasattr(self.__class__, "record_operations")
+
+        if new_api:
+            self._record_operations = collections.defaultdict(
+                lambda: True
+            )
+            self._auto_populate = collections.defaultdict(
+                lambda: auto_populate
+            )
+        else:
+            self.record_operations = True
+            self.auto_populate = auto_populate
+
+        self.cache_key_maker = cache_key_maker
+        if self.cache_key_maker is None:
+            self.cache_key_maker = ftrack_api.cache.StringKeyMaker()
+
+        # Enforce always having a memory cache at top level so that the same
+        # in-memory instance is returned from session.
+        self.cache = ftrack_api.cache.LayeredCache([
+            ftrack_api.cache.MemoryCache()
+        ])
+
+        if cache is not None:
+            if callable(cache):
+                cache = cache(self)
+
+            if cache is not None:
+                self.cache.caches.append(cache)
+
+        if new_api:
+            self.merge_lock = threading.RLock()
+
+        self._managed_request = None
+        self._request = requests.Session()
+        self._request.auth = ftrack_api.session.SessionAuthentication(
+            self._api_key, self._api_user
+        )
+        self.request_timeout = timeout
+
+        # Fetch server information and in doing so also check credentials.
+        self._server_information = self._fetch_server_information()
+
+        # Now check compatibility of server based on retrieved information.
+        self.check_server_compatibility()
+
+        # Construct event hub and load plugins.
+        self._event_hub = self._create_event_hub()
+
+        self._auto_connect_event_hub_thread = None
+        if auto_connect_event_hub:
+            # Connect to event hub in background thread so as not to block main
+            # session usage waiting for event hub connection.
+            self._auto_connect_event_hub_thread = threading.Thread(
+                target=self._event_hub.connect
+            )
+            self._auto_connect_event_hub_thread.daemon = True
+            self._auto_connect_event_hub_thread.start()
+
+        # Register to auto-close session on exit.
+        atexit.register(WeakMethod(self.close))
+
+        self._plugin_paths = plugin_paths
+        if self._plugin_paths is None:
+            self._plugin_paths = os.environ.get(
+                'FTRACK_EVENT_PLUGIN_PATH', ''
+            ).split(os.pathsep)
+
+        self._discover_plugins(plugin_arguments=plugin_arguments)
+
+        # TODO: Make schemas read-only and non-mutable (or at least without
+        # rebuilding types)?
+        if schema_cache_path is not False:
+            if schema_cache_path is None:
+                schema_cache_path = appdirs.user_cache_dir()
+                schema_cache_path = os.environ.get(
+                    'FTRACK_API_SCHEMA_CACHE_PATH', schema_cache_path
+                )
+
+            schema_cache_path = os.path.join(
+                schema_cache_path, 'ftrack_api_schema_cache.json'
+            )
+
+        self.schemas = self._load_schemas(schema_cache_path)
+        self.types = self._build_entity_type_classes(self.schemas)
+
+        ftrack_api._centralized_storage_scenario.register(self)
+
+        self._configure_locations()
+        self.event_hub.publish(
+            ftrack_api.event.base.Event(
+                topic='ftrack.api.session.ready',
+                data=dict(
+                    session=self
+                )
+            ),
+            synchronous=True
+        )
+
+    def _create_event_hub(self):
+        return ftrack_api.event.hub.EventHub(
+            self._server_url,
+            self._api_user,
+            self._api_key
+        )
+
+
+class OPServerSession(CustomEventHubSession):
+    def _create_event_hub(self):
+        return ProcessEventHub(
+            self._server_url,
+            self._api_user,
+            self._api_key
+        )
