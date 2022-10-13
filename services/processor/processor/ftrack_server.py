41cb161b7b73fe7efafaa38a999881e4f029a65a
Jakub Trllo <jakub.trllo@gmail.com>
2022-10-13 11:40:26 +0200

    moved import logic to lib

--- services/processor/processor/ftrack_server.py
+++ services/processor/processor/ftrack_server.py
@@ -1,12 +1,12 @@
 import os
-import sys
 import time
 import types
 import logging
 import traceback
-import importlib
 
 import ftrack_api
+
+from .lib import modules_from_path
 
 """
 # Required - Needed for connection to Ftrack
@@ -24,93 +24,6 @@ FTRACK_EVENTS_PATH # Paths to folders where are located actions
 PYTHONPATH # Path to ftrack_api and paths to all modules used in actions
     - path to ftrack_action_handler, etc.
 """
-
-
-def import_filepath(filepath, module_name=None):
-    """Import python file as python module.
-
-    Python 2 and Python 3 compatibility.
-
-    Args:
-        filepath(str): Path to python file.
-        module_name(str): Name of loaded module. Only for Python 3. By default
-            is filled with filename of filepath.
-    """
-    if module_name is None:
-        module_name = os.path.splitext(os.path.basename(filepath))[0]
-
-    # Make sure it is not 'unicode' in Python 2
-    module_name = str(module_name)
-
-    # Prepare module object where content of file will be parsed
-    module = types.ModuleType(module_name)
-
-    # Use loader so module has full specs
-    module_loader = importlib.machinery.SourceFileLoader(
-        module_name, filepath
-    )
-    module_loader.exec_module(module)
-    return module
-
-
-def modules_from_path(folder_path, log=None):
-    """Get python scripts as modules from a path.
-
-    Arguments:
-        path (str): Path to folder containing python scripts.
-
-    Returns:
-        tuple<list, list>: First list contains successfully imported modules
-            and second list contains tuples of path and exception.
-    """
-    crashed = []
-    modules = []
-    output = (modules, crashed)
-    # Just skip and return empty list if path is not set
-    if not folder_path:
-        return output
-
-    if log is None:
-        log = logging.getLogger("modules_from_path")
-    # Do not allow relative imports
-    if folder_path.startswith("."):
-        log.warning((
-            "BUG: Relative paths are not allowed for security reasons. {}"
-        ).format(folder_path))
-        return output
-
-    folder_path = os.path.normpath(folder_path)
-
-    if not os.path.isdir(folder_path):
-        log.warning("Not a directory path: {}".format(folder_path))
-        return output
-
-    for filename in os.listdir(folder_path):
-        # Ignore files which start with underscore
-        if filename.startswith("_"):
-            continue
-
-        mod_name, mod_ext = os.path.splitext(filename)
-        if not mod_ext == ".py":
-            continue
-
-        full_path = os.path.join(folder_path, filename)
-        if not os.path.isfile(full_path):
-            continue
-
-        try:
-            module = import_filepath(full_path, mod_name)
-            modules.append((full_path, module))
-
-        except Exception:
-            crashed.append((full_path, sys.exc_info()))
-            log.warning(
-                "Failed to load path: \"{0}\"".format(full_path),
-                exc_info=True
-            )
-            continue
-
-    return output
 
 
 class FtrackServer:
--- services/processor/processor/lib/__init__.py
+++ services/processor/processor/lib/__init__.py
@@ -1,8 +1,15 @@
+from .python_module_tools import (
+    modules_from_path,
+    import_filepath,
+)
 from .ftrack_event_handler import BaseEventHandler
 from .ftrack_action_handler import LocalAction, ServerAction
 
 
 __all__ = (
+    "modules_from_path",
+    "import_filepath",
+
     "BaseEventHandler",
 
     "LocalAction",
