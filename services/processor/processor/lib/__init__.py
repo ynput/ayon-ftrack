a70d3f5aaf4df34f30fc0f1e4202587d9f6f5200
Jakub Trllo <jakub.trllo@gmail.com>
2023-03-29 15:25:44 +0200

    removed another duplicated code

--- services/processor/processor/lib/__init__.py
+++ services/processor/processor/lib/__init__.py
@@ -1,14 +1,3 @@
-from .utils import (
-    create_chunks,
-    get_addon_resource_url,
-    get_icon_url,
-)
-
-from .constants import (
-    CUST_ATTR_GROUPS,
-    CUST_ATTR_ID_KEY,
-    CUST_ATTR_AUTO_SYNC,
-)
 from .python_module_tools import (
     modules_from_path,
     import_filepath,
@@ -16,14 +5,6 @@ from .python_module_tools import (
 
 
 __all__ = (
-    "create_chunks",
-    "get_addon_resource_url",
-    "get_icon_url",
-
-    "CUST_ATTR_GROUPS",
-    "CUST_ATTR_ID_KEY",
-    "CUST_ATTR_AUTO_SYNC",
-
     "modules_from_path",
     "import_filepath",
 )
