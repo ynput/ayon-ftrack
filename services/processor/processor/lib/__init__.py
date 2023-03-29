5fdf95979d97440467c0191d3e69ccf2363fd8a6
Jakub Trllo <jakub.trllo@gmail.com>
2023-03-29 14:42:59 +0200

    removed unused/duplicated code

--- services/processor/processor/lib/__init__.py
+++ services/processor/processor/lib/__init__.py
@@ -14,9 +14,6 @@ from .python_module_tools import (
     import_filepath,
 )
 
-from .ftrack_event_handler import BaseEventHandler
-from .ftrack_action_handler import LocalAction, ServerAction
-
 
 __all__ = (
     "create_chunks",
@@ -29,9 +26,4 @@ __all__ = (
 
     "modules_from_path",
     "import_filepath",
-
-    "BaseEventHandler",
-
-    "LocalAction",
-    "ServerAction",
 )
