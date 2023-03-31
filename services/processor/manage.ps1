cc9caed3d9db5f3cc396491591e1fad8e886fa3a
Jakub Trllo <jakub.trllo@gmail.com>
2023-03-31 17:54:00 +0200

    prepared manage script for processor service

---
+++ services/processor/manage.ps1
@@ -0,0 +1,62 @@
+# Receive first positional argument
+Param([Parameter(Position=0)]$FunctionName)
+$image = "ynput/ayon-ftrack-processor:0.0.1"
+$current_dir = Get-Location
+$script_dir_rel = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+$script_dir = (Get-Item $script_dir_rel).FullName
+
+function defaultfunc {
+  Write-Host ""
+  Write-Host "*************************"
+  Write-Host "AYON ftrack processor service"
+  Write-Host "   Run event processing service"
+  Write-Host "*************************"
+  Write-Host ""
+  Write-Host "Run event processor as a service."
+  Write-Host ""
+  Write-Host "Usage: manage [target]"
+  Write-Host ""
+  Write-Host "Runtime targets:"
+  Write-Host "  build    Build docker image"
+  Write-Host "  clean    Remove docker image"
+  Write-Host "  dev      Run docker (for development purposes)"
+}
+
+function build {
+  & cp -r "$current_dir/../../ftrack_common" .
+  & docker build -t "$image" .
+  & Remove-Item -Recurse -Force "$current_dir/ftrack_common"
+}
+
+function clean {
+  & docker rmi $(image)
+}
+
+function dev {
+  & cp -r "$current_dir/../../ftrack_common" .
+  & docker run --rm -ti `
+    -v "$($current_dir):/service" `
+  	--hostname ftrackproc `
+  	--env AY_API_KEY="verysecureapikey" `
+  	--env AY_SERVER_URL="http://localhost:5000" `
+  	--env AY_ADDON_NAME=ftrack `
+  	--env AY_ADDON_VERSION=0.0.1 `
+  	"$($image)" python -m processor
+  & Remove-Item -Recurse -Force "$current_dir/ftrack_common"
+}
+
+function main {
+  if ($FunctionName -eq "build") {
+    build
+  } elseif ($FunctionName -eq "clean") {
+    clean
+  } elseif ($FunctionName -eq "dev") {
+    dev
+  } elseif ($FunctionName -eq $null) {
+    defaultfunc
+  } else {
+    Write-Host "Unknown function ""$FunctionName"""
+  }
+}
+
+main
