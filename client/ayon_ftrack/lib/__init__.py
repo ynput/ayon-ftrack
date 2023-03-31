e11549af00a954bd5675b68d5a31f6a2730abf3d
Jakub Trllo <jakub.trllo@gmail.com>
2023-03-31 12:35:46 +0200

    removed constants from ayon ftrack

--- client/ayon_ftrack/lib/__init__.py
+++ client/ayon_ftrack/lib/__init__.py
@@ -1,25 +1,8 @@
-from .constants import (
-    CUST_ATTR_ID_KEY,
-    CUST_ATTR_AUTO_SYNC,
-    CUST_ATTR_GROUP,
-    CUST_ATTR_TOOLS,
-    CUST_ATTR_APPLICATIONS,
-    CUST_ATTR_INTENT,
-    FPS_KEYS
-)
 from . import credentials
 from .utils import statics_icon
 
 
 __all__ = (
-    "CUST_ATTR_ID_KEY",
-    "CUST_ATTR_AUTO_SYNC",
-    "CUST_ATTR_GROUP",
-    "CUST_ATTR_TOOLS",
-    "CUST_ATTR_APPLICATIONS",
-    "CUST_ATTR_INTENT",
-    "FPS_KEYS",
-
     "credentials",
 
     "statics_icon"
