b953e05223fc8b289ea766e35a3755741ad2a6c8
Jakub Trllo <jakub.trllo@gmail.com>
2023-08-01 13:31:05 +0200

    add ability to read secrets when starting services

--- services/processor/processor/server.py
+++ services/processor/processor/server.py
@@ -1,6 +1,7 @@
 import os
 import logging
 
+import ayon_api
 from ayon_api import get_addons_studio_settings, init_service
 from ftrack_common import FtrackServer
 
@@ -15,6 +16,18 @@ def get_handler_paths() -> list[str]:
     return handler_paths
 
 
+def get_secrets():
+    """Backwards compatibility for older ayon api versions.
+
+    Returns:
+        list[dict[str, str]]: List of secrets from server.
+    """
+
+    if hasattr(ayon_api, "get_secrets"):
+        return ayon_api.get_secrets()
+    return ayon_api.get("secrets").data
+
+
 def main():
     logging.basicConfig()
 
@@ -24,10 +37,23 @@ def main():
     settings = get_addons_studio_settings()
     ftrack_settings = settings["ftrack"]
     service_settings = ftrack_settings["service_settings"]
+
+    secrets_by_name = {
+        secret["name"]: secret["value"]
+        for secret in get_secrets()
+    }
+    api_key = service_settings["api_key"]
+    username = service_settings["username"]
+    if api_key in secrets_by_name:
+        api_key = secrets_by_name[api_key]
+
+    if username in secrets_by_name:
+        username = secrets_by_name[username]
+
     session = AYONServerSession(
         ftrack_settings["ftrack_server"],
-        service_settings["api_key"],
-        service_settings["username"],
+        api_key,
+        username,
         auto_connect_event_hub=False
     )
     server = FtrackServer(handler_paths)
