6efc0c152918346a897adfecdfb1a48b8a43c35a
Jakub Trllo <jakub.trllo@gmail.com>
2023-08-16 16:07:50 +0200

    added missing settings

--- server/settings/publish_plugins.py
+++ server/settings/publish_plugins.py
@@ -224,6 +224,66 @@ class IntegrateFtrackFarmStatusModel(BaseSettingsModel):
     )
 
 
+class FtrackTaskStatusProfile(BaseSettingsModel):
+    host_names: list[str] = Field(
+        default_factory=list,
+        title="Host names",
+    )
+    task_types: list[str] = Field(
+        default_factory=list,
+        title="Task types",
+    )
+    task_names: list[str] = Field(
+        default_factory=list,
+        title="Task names",
+    )
+    families: list[str] = Field(
+        default_factory=list,
+        title="Families",
+    )
+    subset_names: list[str] = Field(
+        default_factory=list,
+        title="Subset names",
+    )
+    status_name: str = Field(
+        "",
+        title="Status name"
+    )
+
+
+class FtrackTaskStatusLocalModel(BaseSettingsModel):
+    _isGroup = True
+    status_profiles: list[FtrackTaskStatusProfile] = Field(
+        title="Status profiles",
+        default_factory=list,
+        description="Change status of task when is integrated locally"
+    )
+
+
+class FtrackTaskStatusOnFarmModel(BaseSettingsModel):
+    _isGroup = True
+    status_profiles: list[FtrackTaskStatusProfile] = Field(
+        title="Status profiles",
+        default_factory=list,
+        description=(
+            "Change status of task when it's subset is integrated on farm"
+        )
+    )
+
+
+class IntegrateFtrackTaskStatusModel(BaseSettingsModel):
+    _isGroup = True
+    after_version_statuses: bool = Field(
+        True,
+        title="After version integration",
+        description=(
+            "Apply collected task statuses. This plugin can run before or"
+            " after version integration. Some status automations may conflict"
+            " with status changes on versions because of wrong order."
+        )
+    )
+
+
 class FtrackPublishPlugins(BaseSettingsModel):
     """Settings for event handlers running in ftrack service."""
 
@@ -276,6 +336,18 @@ class FtrackPublishPlugins(BaseSettingsModel):
         description=(
             "Change status of task when it's subset is submitted to farm"
         ),
+    )
+    ftrack_task_status_local_publish: FtrackTaskStatusLocalModel = Field(
+        default_factory=FtrackTaskStatusLocalModel,
+        title="Ftrack Status Local Integration",
+    )
+    ftrack_task_status_on_farm_publish: FtrackTaskStatusOnFarmModel = Field(
+        default_factory=FtrackTaskStatusOnFarmModel,
+        title="Ftrack Status On Farm Integration",
+    )
+    IntegrateFtrackTaskStatus: IntegrateFtrackTaskStatusModel = Field(
+        default_factory=IntegrateFtrackTaskStatusModel,
+        title="Integrate Ftrack Task Status"
     )
 
 
@@ -613,6 +685,28 @@ DEFAULT_PUBLISH_SETTINGS = {
         "additional_metadata_keys": []
     },
     "IntegrateFtrackFarmStatus": {
-        "farm_status_profiles": []
+        "farm_status_profiles": [
+            {
+                "hosts": [
+                    "celaction"
+                ],
+                "task_types": [],
+                "task_names": [],
+                "families": [
+                    "render"
+                ],
+                "subsets": [],
+                "status_name": "Render"
+            }
+        ]
+    },
+    "ftrack_task_status_local_publish": {
+        "status_profiles": []
+    },
+    "ftrack_task_status_on_farm_publish": {
+        "status_profiles": []
+    },
+    "IntegrateFtrackTaskStatus": {
+        "after_version_statuses": True
     }
 }
