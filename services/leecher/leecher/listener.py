8f7f962f0bf1d45c7b353fd078d3b7eebdf618d8
Jakub Trllo <jakub.trllo@gmail.com>
2023-09-15 11:38:12 +0200

    better handling of services errors

--- services/leecher/leecher/listener.py
+++ services/leecher/leecher/listener.py
@@ -2,7 +2,9 @@ import sys
 import time
 import signal
 import logging
-from typing import Any, Callable, Union
+import threading
+import traceback
+from typing import Any, Union
 
 import ftrack_api
 import ayon_api
@@ -39,7 +41,7 @@ def callback(event):
     log.info(f"Stored event {event_data['topic']}")
 
 
-def _trigger_leecher_started_event(session):
+def _trigger_leecher_started_event(session: ftrack_api.Session):
     user = session.query(
         "User where username is \"{}\"".format(session.api_user)
     ).one()
@@ -58,70 +60,154 @@ def _trigger_leecher_started_event(session):
     )
 
 
-def listen_loop(session, callback):
-    while not session.event_hub.connected:
-        time.sleep(0.1)
-
-    _trigger_leecher_started_event(session)
-    session.event_hub.subscribe("topic=*", callback)
-    session.event_hub.wait()
+class _GlobalContext:
+    stop_event = threading.Event()
+    session = None
+    session_fail_logged = 0
 
 
-def get_secrets():
-    """Backwards compatibility for older ayon api versions.
-
-    Returns:
-        list[dict[str, str]]: List of secrets from server.
-    """
-
-    if hasattr(ayon_api, "get_secrets"):
-        return ayon_api.get_secrets()
-    return ayon_api.get("secrets").data
+def get_service_label() -> str:
+    return " ".join([
+        ayon_api.get_service_addon_name(),
+        ayon_api.get_service_addon_version()
+    ])
 
 
-def main(func: Union[Callable, None] = None):
-    logging.basicConfig(level=logging.INFO)
-
-    log.info("Starting listener")
-    if func is None:
-        func = callback
-
-    ayon_api.init_service()
-    settings = ayon_api.get_service_addon_settings()
+def _create_session():
+    ftrack_settings = ayon_api.get_service_addon_settings()
+    ftrack_url = ftrack_settings["ftrack_server"]
+    service_settings = ftrack_settings["service_settings"]
 
     secrets_by_name = {
         secret["name"]: secret["value"]
-        for secret in get_secrets()
+        for secret in ayon_api.get_secrets()
     }
-    ftrack_url = settings["ftrack_server"]
-    api_key = settings["service_settings"]["api_key"]
-    username = settings["service_settings"]["username"]
+    api_key = service_settings["api_key"]
+    username = service_settings["username"]
     if api_key in secrets_by_name:
         api_key = secrets_by_name[api_key]
 
     if username in secrets_by_name:
         username = secrets_by_name[username]
 
-    log.debug("Creating ftrack session")
-    session = ftrack_api.Session(
+    if not username or not api_key:
+        return (
+            "Missing Ftrack Username or Ftrack API in settings."
+            f" Please check your settings of {get_service_label()}."
+        )
+
+    _GlobalContext.session = ftrack_api.Session(
         ftrack_url.strip("/ "),
         api_key,
         username,
         auto_connect_event_hub=True,
     )
 
+
+def create_session():
+    tb_content = None
+    error_summary = "Failed to create ftrack session."
+    try:
+        error_message = _create_session()
+    except ftrack_api.exception.ServerError:
+        error_message = (
+            "Ftrack Username or ftrack API in settings are not valid."
+            f" Please check your settings of {get_service_label()}."
+        )
+    except Exception:
+        error_message = f"{error_summary} Crashed!!!"
+        tb_lines = traceback.format_exception(*sys.exc_info())
+        tb_content = "".join(tb_lines)
+
+    session = _GlobalContext.session
+    if session is not None:
+        print("Created ftrack session")
+        return session
+
+    if not error_message:
+        error_message = error_summary
+    print(error_message)
+    if tb_content:
+        print(tb_content)
+    if (
+        (tb_content is not None and _GlobalContext.session_fail_logged == 2)
+        or (tb_content is None and _GlobalContext.session_fail_logged == 1)
+    ):
+        return
+
+    event_data = {
+        "sender": ayon_api.ServiceContext.service_name,
+        "finished": True,
+        "store": True,
+        "description": "ftrack leecher error",
+        "payload": {
+            "message": error_message,
+        }
+    }
+    if tb_lines is None:
+        _GlobalContext.session_fail_logged = 2
+        event_data["summary"] = error_summary
+
+    else:
+        _GlobalContext.session_fail_logged = 1
+        event_data["payload"]["traceback"] = tb_content
+
+    ayon_api.dispatch_event(
+        "log.error",
+        **event_data,
+    )
+
+
+def main_loop():
+    while not _GlobalContext.stop_event.is_set():
+        session = create_session()
+        if session is None:
+            time.sleep(10)
+            continue
+
+        _GlobalContext.session_fail_logged = False
+
+        while not session.event_hub.connected:
+            time.sleep(0.1)
+
+        _trigger_leecher_started_event(session)
+        session.event_hub.subscribe("topic=*", callback)
+        session.event_hub.wait()
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    try:
+        ayon_api.init_service()
+        connected = True
+    except Exception:
+        connected = False
+
+    if not connected:
+        print("Failed to connect to AYON server.")
+        # Sleep for 10 seconds, so it is possible to see the message in
+        #   docker
+        # NOTE: Becuase AYON connection failed, there's no way how to log it
+        #   to AYON server (obviously)... So stdout is all we have.
+        time.sleep(10)
+        sys.exit(1)
+
+    print("Connected to AYON server.")
+
     # Register interrupt signal
     def signal_handler(sig, frame):
-        log.warning("Process stop requested. Terminating process.")
-        if session.event_hub.connected is True:
-            session.event_hub.disconnect()
-        session.close()
-        log.warning("Termination finished.")
+        print("Process stop requested. Terminating process.")
+        _GlobalContext.stop_event.set()
+        session = _GlobalContext.session
+        if session is not None:
+            if session.event_hub.connected is True:
+                session.event_hub.disconnect()
+            session.close()
+        print("Termination finished.")
         sys.exit(0)
 
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
 
-    log.info("Main loop starting")
-    sys.exit(listen_loop(session, func))
-    log.info("Process stopped")
+    main_loop()
--- services/processor/processor/server.py
+++ services/processor/processor/server.py
@@ -1,11 +1,23 @@
 import os
+import sys
+import threading
+import time
 import logging
+import signal
+import traceback
 
 import ayon_api
-from ayon_api import get_addons_studio_settings, init_service
+import ftrack_api
+
 from ftrack_common import FtrackServer
 
 from .ftrack_session import AYONServerSession
+
+
+class _GlobalContext:
+    stop_event = threading.Event()
+    session = None
+    session_fail_logged = 0
 
 
 def get_handler_paths() -> list[str]:
@@ -16,32 +28,21 @@ def get_handler_paths() -> list[str]:
     return handler_paths
 
 
-def get_secrets():
-    """Backwards compatibility for older ayon api versions.
-
-    Returns:
-        list[dict[str, str]]: List of secrets from server.
-    """
-
-    if hasattr(ayon_api, "get_secrets"):
-        return ayon_api.get_secrets()
-    return ayon_api.get("secrets").data
+def get_service_label():
+    return " ".join([
+        ayon_api.get_service_addon_name(),
+        ayon_api.get_service_addon_version()
+    ])
 
 
-def main():
-    logging.basicConfig(level=logging.INFO)
-
-    init_service()
-
-    handler_paths = get_handler_paths()
-
+def _create_session():
     ftrack_settings = ayon_api.get_service_addon_settings()
     ftrack_url = ftrack_settings["ftrack_server"]
     service_settings = ftrack_settings["service_settings"]
 
     secrets_by_name = {
         secret["name"]: secret["value"]
-        for secret in get_secrets()
+        for secret in ayon_api.get_secrets()
     }
     api_key = service_settings["api_key"]
     username = service_settings["username"]
@@ -51,11 +52,123 @@ def main():
     if username in secrets_by_name:
         username = secrets_by_name[username]
 
-    session = AYONServerSession(
+    if not username or not api_key:
+        return (
+            "Missing Ftrack Username or Ftrack API in settings."
+            f" Please check your settings of {get_service_label()}."
+        )
+
+    _GlobalContext.session = AYONServerSession(
         ftrack_url.strip("/ "),
         api_key,
         username,
         auto_connect_event_hub=False
     )
-    server = FtrackServer(handler_paths)
-    server.run_server(session)
+
+
+def create_session():
+    tb_content = None
+    error_summary = "Failed to create ftrack session."
+    try:
+        error_message = _create_session()
+    except ftrack_api.exception.ServerError:
+        error_message = (
+            "Ftrack Username or ftrack API in settings are not valid."
+            f" Please check your settings of {get_service_label()}."
+        )
+    except Exception:
+        error_message = f"{error_summary} Crashed!!!"
+        tb_lines = traceback.format_exception(*sys.exc_info())
+        tb_content = "".join(tb_lines)
+
+    session = _GlobalContext.session
+    if session is not None:
+        print("Created ftrack session")
+        return session
+
+    if not error_message:
+        error_message = error_summary
+    print(error_message)
+    if tb_content:
+        print(tb_content)
+    if (
+        (tb_content is not None and _GlobalContext.session_fail_logged == 2)
+        or (tb_content is None and _GlobalContext.session_fail_logged == 1)
+    ):
+        return
+
+    event_data = {
+        "sender": ayon_api.ServiceContext.service_name,
+        "finished": True,
+        "store": True,
+        "description": "ftrack processor error",
+        "payload": {
+            "message": error_message,
+        }
+    }
+    if tb_lines is None:
+        _GlobalContext.session_fail_logged = 2
+        event_data["summary"] = error_summary
+
+    else:
+        _GlobalContext.session_fail_logged = 1
+        event_data["payload"]["traceback"] = tb_content
+
+    ayon_api.dispatch_event(
+        "log.error",
+        **event_data,
+    )
+
+
+def main_loop():
+    while not _GlobalContext.stop_event.is_set():
+        session = create_session()
+        if session is None:
+            time.sleep(10)
+            continue
+
+        _GlobalContext.session_fail_logged = False
+
+        handler_paths = get_handler_paths()
+
+        server = FtrackServer(handler_paths)
+        print("Starting listening loop")
+        server.run_server(session)
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    try:
+        ayon_api.init_service()
+        connected = True
+    except Exception:
+        connected = False
+
+    if not connected:
+        print("Failed to connect to AYON server.")
+        # Sleep for 10 seconds, so it is possible to see the message in
+        #   docker
+        # NOTE: Becuase AYON connection failed, there's no way how to log it
+        #   to AYON server (obviously)... So stdout is all we have.
+        time.sleep(10)
+        sys.exit(1)
+
+    print("Connected to AYON server.")
+
+    # Register interrupt signal
+    def signal_handler(sig, frame):
+        print("Process stop requested. Terminating process.")
+        _GlobalContext.stop_event.set()
+        session = _GlobalContext.session
+        if session is not None:
+            if session.event_hub.connected is True:
+                session.event_hub.disconnect()
+            session.close()
+        print("Termination finished.")
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
+    main_loop()
