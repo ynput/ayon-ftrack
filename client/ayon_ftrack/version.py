a48d0730b8f14216305ab61f2bd800efc2f4c4c7
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-06-18 15:34:55 +0200

    bump version to 1.1.7-dev.1

--- client/ayon_ftrack/version.py
+++ client/ayon_ftrack/version.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 """Package declaring ftrack addon version."""
-__version__ = "1.1.6"
+__version__ = "1.1.7-dev.1"
--- package.py
+++ package.py
@@ -1,5 +1,5 @@
 name = "ftrack"
-version = "1.1.6"
+version = "1.1.7-dev.1"
 title = "Ftrack"
 client_dir = "ayon_ftrack"
 
